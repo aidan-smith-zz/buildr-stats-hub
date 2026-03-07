35648de98252e33a9516194761757df19f78583f
Aidan Smith <aidan.smith1@hotmail.co.uk>
2026-03-07 06:09:21 +0000

    reducing crawling a bit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,13 @@ const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "https://statsbuildr.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/"],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
